d354911d836ad7cba335f4144e16fce624d922e5
hlyates <hlyates@ksu.edu>
2019-12-01 22:54:28 -0600

    Adding a project that explores testthat package

---
+++ test_that_project/main.R
@@ -0,0 +1,40 @@
+#################################################################################
+#Title: test that project                                                       #
+#Author: Heath Yates                                                            # 
+#Date: December 1, 2019                                                         # 
+#Updated:                                                                       #  
+#Description:                                                                   # 
+#References:                                                                    #  
+#################################################################################
+
+
+#################################################################################
+#Section: Libraies and dependencies                                             # 
+#################################################################################
+
+#################################################################################
+#Section: Main function                                                         # 
+#################################################################################
+main <- function(){
+  
+}
+
+main()
+
+
+######################NOTES###########################
+#TODO(HEATH)
+
+#Web api to obtain squirrel data (functions)
+
+#Function to transform data (with custom generic functions)
+
+#Functions to do some modeling (with custom generic functions)
+
+#Write testthat for each file and test generic functions for each 
+
+#Write an rmarkdown report squirrel happenenings
+
+
+#TODO(Start the set up talk with gifs)
+#TODO(Write the talk in Rmarkdown powerpoint)
